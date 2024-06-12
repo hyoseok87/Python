1cb2f532f89ffe2af7551c261f6bc18eabbb0e6f
hyoseok87 <152912333+hyoseok87@users.noreply.github.com>
2024-06-12 07:42:58 +0200

    day6

---
+++ Day6/day6.py
@@ -0,0 +1,19 @@
+#Funktion und Schleifen
+
+print("Hello")
+num_char = len("Hello")
+print(num_char)
+
+#Funktion erstellen
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+# Funktion? Do this / Then do this /Finally do this
+# Calling Functions  my_function()
+
+# Practice : Reeborg's World
+#Indentation
+
