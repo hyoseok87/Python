93edfa0740fc144c0234e535b181e3c240f6def4
hyoseok87 <152912333+hyoseok87@users.noreply.github.com>
2024-06-12 10:26:56 +0200

    day6

--- Day6/day6.py
+++ Day6/day6.py
@@ -14,6 +14,12 @@ my_function()
 # Funktion? Do this / Then do this /Finally do this
 # Calling Functions  my_function()
 
-# Practice : Reeborg's World
-#Indentation
 
+#Indentation(들여쓰기)
+# for Loop Do something to each item
+# while Loop Do something repeatedly
+#While something_is_true: Do this / then do this / then do this
+# when something_is_false is: while Loop is end
+#Infinite Loop
+
+# Practice : Reeborg's World
