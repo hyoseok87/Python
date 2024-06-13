343068f2fa1a03e5feefcfd84664ed34d1ae5d9b
hyoseok87 <152912333+hyoseok87@users.noreply.github.com>
2024-06-13 14:07:38 +0200

    day7

---
+++ day7/hangman.py
@@ -0,0 +1,27 @@
+import random
+
+
+german_words = [
+    "Apfel", "Bäckerei", "Computer", "Dach", "Elefant", "Fahrrad", "Garten", "Haus",
+    "Insel", "Jacke", "Katze", "Lampe", "Musik", "Nacht", "Orange", "Pizza", "Quark",
+    "Regen", "Schule", "Tisch", "Uhr", "Vogel", "Wasser", "Xylophon", "Yacht",
+    "Zitrone", "Bahnhof", "Chirurg", "Dünger", "Eltern", "Freund", "Gemüse", "Hund",
+    "Insel", "Jäger", "Kaffee", "Lehrer", "Milch", "Nudeln", "Ofen", "Pferd", "Qualle",
+    "Rakete", "Sonne", "Tee", "Urlaub", "Vogel", "Würfel", "Zahnarzt", "Zwilling"
+]
+
+
+random_word = random.choice(german_words)
+
+print(f'Pssst, the solution is {random_word}.')
+word_length = len(random_word)
+display = ["_"] * word_length
+print(display)
+guess = input("Guess a letter: ").lower()
+
+for position in range(word_length):
+    letter = random_word[position]
+    if letter == guess:
+        display[position] = letter
+    else:
+        print("Wrong")
