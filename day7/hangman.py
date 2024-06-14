2b45cd2d82072de556d1fa8b72e02ce6d93824be
hyoseok87 <152912333+hyoseok87@users.noreply.github.com>
2024-06-14 09:29:26 +0200

    day7 version3

--- day7/hangman.py
+++ day7/hangman.py
@@ -1,5 +1,74 @@
 import random
 
+logo = r''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+stages = [r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', r'''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
 german_words = [
     "Apfel", "Bäckerei", "Computer", "Dach", "Elefant", "Fahrrad", "Garten", "Haus",
     "Insel", "Jacke", "Katze", "Lampe", "Musik", "Nacht", "Orange", "Pizza", "Quark",
@@ -9,18 +78,46 @@ german_words = [
     "Rakete", "Sonne", "Tee", "Urlaub", "Vogel", "Würfel", "Zahnarzt", "Zwilling"
 ]
 
-random_word = random.choice(german_words)
+while True:
+    print(logo)
+    random_word = random.choice(german_words)
+    print(f'Pssst, the solution is {random_word}.')
+    word_length = len(random_word)
+    display = ["_"] * word_length
+    lives = 6
 
-print(f'Pssst, the solution is {random_word}.')
-word_length = len(random_word)
-display = ["_"] * word_length
-print(display)
-
-while "_" in display:
-    guess = input("Guess a letter: ").lower()
-    for position in range(word_length):
-        letter = random_word[position].lower()
-        if letter == guess:
-            display[position] = random_word[position]
+    while "_" in display:
+        guess = input("Guess a letter: ").lower()
+        if guess in random_word.lower():
+            for position in range(word_length):
+                letter = random_word[position].lower()
+                if letter == guess:
+                    display[position] = random_word[position]
             print(display)
-print("Win")
+        else:
+            lives -= 1
+            print(stages[lives])
+            print(f"Your lives are {lives}")
+            print(display)
+            if lives == 0:
+                print("You lose")
+                break
+
+    if "_" not in display:
+        print("You won!")
+
+    request = input("Do you want to play again? (y/n): ").lower()
+    if request == "n":
+        print("See you again")
+        break
+    elif request != "y":
+        while True:
+            print("Write a valid answer")
+            request = input("Do you want to play again? (y/n): ").lower()
+            if request == "y":
+                break
+            elif request == "n":
+                break
+        if request == "n":
+            print("See you again")
+            break
